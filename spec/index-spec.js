fa32dfecfbc9412df30821b4370000c2d34b3da5
tfennelly <tom.fennelly@gmail.com>
2015-08-08 00:44:39 +0100

    Added a comment to a test

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -218,7 +218,7 @@ describe("index.js", function () {
             // The require should "trigger" the loading of the module.
             // Should pass because export will happen before the timeout
             jenkins.setRegisterTimeout(2000);
-            // 2 modules, the first is in the global namespace and the send is in a 
+            // 2 modules, the first is in the global namespace and the second is in a 
             // plugin namespace ('pluginB')
             jenkins.import('mathUtils', 'pluginB:timeUtils')
                 .onFulfilled(function(mathUtils, timeUtils) {
