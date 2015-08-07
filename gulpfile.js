1814e2d19784774a97ee9e519510e05ec440265c
tfennelly <tom.fennelly@gmail.com>
2015-08-07 13:44:29 +0100

    Replace gulp-jasmine-phantom with plain gulp-jasmine

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
-var jasmine = require('gulp-jasmine-phantom');
+var jasmine = require('gulp-jasmine');
+var jasmineReporters = require('jasmine-reporters');
+        
+var terminalReporter = new jasmineReporters.TerminalReporter({
+    verbosity: 3,
+    color: true,
+    showStack: true
+});        
+var junitReporter = new jasmineReporters.JUnitXmlReporter({
+    savePath: 'target/surefire-reports',
+    consolidateAll: true,
+    filePrefix: 'JasmineReport'    
+});
  
 gulp.task('default', function () {
     return gulp.src('spec/*-spec.js')
-        .pipe(jasmine());
+        .pipe(jasmine({reporter: [terminalReporter, junitReporter]}));
 });
